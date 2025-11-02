8e9bb56fec008f0a12b4d5b872f7f622d5933599
kittu2karthik <71444538+kittu2karthik@users.noreply.github.com>
2025-11-03 01:27:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.addEventListener('load', () => {
+    const time = document.createElement('p');
+    time.id = 'timer';
+    document.body.appendChild(time);
+    
+    setInterval(() => {
+        time.innerText = new Date().toLocaleString();
+    }, 1000);
+});
